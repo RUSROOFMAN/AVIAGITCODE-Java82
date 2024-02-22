ceee68f2b5072543a92c8a0c328789ad5322b547
Sergey Bondarev <bondarevsergeq1@mail.ru>
2024-02-22 21:18:48 +0700

    Change code

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,9 @@
 public class Main {
     public static void main(String[] args) {
 
-        int amount = 14597; // Сумма покупки
-        int money = 20; // Кол-во рублей для начисления одной мили
+        int amount = 14258; // Сумма покупки
+        int money = 20; // Кол-во рублей для начисления одной(1) мили
         int bonus = amount / money * 1; // Формула начисления бонусных миль (за каждые 20р - начисляется 1 миля)
         System.out.println("Итоговые бонусные мили: " + bonus);
-    }  }
+    }
+}
